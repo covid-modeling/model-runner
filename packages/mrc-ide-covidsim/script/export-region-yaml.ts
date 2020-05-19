e83e86cbd21a8bff34345599b5973a0904065dca
Greg Orzell <gorzell@github.com>
2020-05-19 09:03:47 +0200

    script: Add sorting.

--- packages/mrc-ide-covidsim/script/export-region-yaml.ts
+++ packages/mrc-ide-covidsim/script/export-region-yaml.ts
@@ -1,8 +1,10 @@
 import { COUNTRY_PARAMS_BY_ISO_CODE } from '../src/mappings'
 import * as YAML from 'yaml'
 
-const output = Object.keys(COUNTRY_PARAMS_BY_ISO_CODE).sort().map(k => {
-  return { [k]: Object.keys(COUNTRY_PARAMS_BY_ISO_CODE[k].subregions).sort() }
-})
+const output = Object.keys(COUNTRY_PARAMS_BY_ISO_CODE)
+  .sort()
+  .map(k => {
+    return { [k]: Object.keys(COUNTRY_PARAMS_BY_ISO_CODE[k].subregions).sort() }
+  })
 
 console.log(YAML.stringify(output))
--- packages/neherlab-covid-19-scenarios/script/export-region-yaml.ts
+++ packages/neherlab-covid-19-scenarios/script/export-region-yaml.ts
@@ -1,8 +1,10 @@
 import { REGION_DATA } from '../src/regions'
 import * as YAML from 'yaml'
 
-const output = Object.keys(REGION_DATA).sort().map(k => {
-  return { [k]: Object.keys(REGION_DATA[k].regions ?? {}).sort() }
-})
+const output = Object.keys(REGION_DATA)
+  .sort()
+  .map(k => {
+    return { [k]: Object.keys(REGION_DATA[k].regions ?? {}).sort() }
+  })
 
 console.log(YAML.stringify(output))
