066ad42f71bb6a5c35aef73bc2ec3a41ce31a616
Greg Orzell <gorzell@github.com>
2020-05-19 08:44:44 +0200

    script: Fix formating and add support to Makefile.

--- packages/neherlab-covid-19-scenarios/script/export-region-yaml.ts
+++ packages/neherlab-covid-19-scenarios/script/export-region-yaml.ts
@@ -1,10 +1,8 @@
-import { REGION_DATA} from '../src/regions'
+import { REGION_DATA } from '../src/regions'
 import * as YAML from 'yaml'
 
-const keys = Object.keys(REGION_DATA)
-console.log(keys)
-const output = keys.map( k => {
-  return { [k]: Object.keys(REGION_DATA[k].regions??{})}
+const output = Object.keys(REGION_DATA).map(k => {
+  return { [k]: Object.keys(REGION_DATA[k].regions ?? {}) }
 })
 
 console.log(YAML.stringify(output))
